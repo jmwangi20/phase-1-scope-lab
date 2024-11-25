cbf6079603990710b8be7cb595d8e401c5f20d1e
John Kihugo Mwangi <johnmwangi13119@gmail.com>
2024-11-25 13:42:06 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(name){
+    customerName=customerName.toUpperCase(name);
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+  }
+const leastFavouriteCustomer = "John Kihugo Mwangi";
+
+function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer="James Ngure Mwangi"
+    return leastFavouriteCustomer;
+}
